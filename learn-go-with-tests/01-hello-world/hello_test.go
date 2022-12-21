bd569149943bf79d81b0b6e3f80d0054edd7f347
StinkyBenji <ether852@gmail.com>
2022-12-21 16:46:44 +0800

    LGwT: hello v2

--- learn-go-with-tests/01-hello-world/hello_test.go
+++ learn-go-with-tests/01-hello-world/hello_test.go
@@ -3,9 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello World"
+	t.Run("Saying hello to people", func(t *testing.T) {
 
+		got := Hello("Joe")
+		want := "Hello, Joe"
+		assertCorrectMessages(t, got, want)
+	})
+
+	// Subtest
+	t.Run("say 'hello world when an empty string is supplied",
+		func(t *testing.T) {
+			got := Hello("")
+			want := "Hello, World"
+			assertCorrectMessages(t, got, want)
+
+		})
+}
+
+// testing.TB is an interface that *testing.T and *testing.B both satisfy
+// t.Helper() func assertCorrectMessages(t testing.TB, got, want string) {
+func assertCorrectMessages(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
