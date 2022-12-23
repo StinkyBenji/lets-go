7f1010c77d792855682da47bcb7bc2e505b55c1f
StinkyBenji <ether852@gmail.com>
2022-12-23 15:50:55 +0800

    LGwT: sumv1

---
+++ learn-go-with-tests/04-arrays-slices/sum.go
@@ -0,0 +1,12 @@
+package main
+
+// [N/...]type{value ...}
+// %v to print default format
+
+func Sum(numbers [5]int) int {
+	sum := 0
+	for i := 0; i < len(numbers); i++ {
+		sum += numbers[i]
+	}
+	return sum
+}
